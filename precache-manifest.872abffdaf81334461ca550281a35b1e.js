aa5834c79b56de789ac90e4a28f6e5925524e112
niraj <nirajntr@gmail.com>
2019-11-12 04:46:18 +0530

    Updates

---
+++ precache-manifest.872abffdaf81334461ca550281a35b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f446e6150898af5d1e79d0051d05d060",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0423f3f9076fecf6a85d",
+    "url": "/monsters-rolodex/static/css/main.9f4eb0b2.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "0423f3f9076fecf6a85d",
+    "url": "/monsters-rolodex/static/js/main.c5c62b7a.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
